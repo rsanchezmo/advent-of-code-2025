c98e8e662ced5cf532a556c8ac9c7488b13f0473
Rodrigo Sánchez Molina <rsanchezm98@gmail.com>
2025-12-01 21:53:34 +0100

    add day02 template

---
+++ day02/solution.py
@@ -0,0 +1,26 @@
+"""Advent of Code 2025 - Day 2"""
+from aoc.base_solution import BaseSolution
+
+
+class Day02(BaseSolution):
+    def __init__(self):
+        super().__init__(day=2)
+
+    def parse_input(self, raw_input: str):
+        return []
+
+
+    def part1(self, input_data) -> int:
+        return 0
+
+    def part2(self, input_data) -> int:
+        return 0
+
+
+if __name__ == "__main__":
+    solution = Day02()
+
+    test_input = """
+"""
+    solution.test(test_input, expected1=None, expected2=None)
+    solution.solve()
